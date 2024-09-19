0cb09cae43d83df77b34bf2b92a8a91736fd7622
Pnidhi14 <pnidhi301@rku.ac.in>
2024-09-19 13:12:18 +0530

    Car_ rental_website

---
+++ fpdf/Runner/Profile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:profile_vk_app/Widgets/info_crick.dart';
+
+final url = "WWW.Virat.Com";
+final email = "Virat@gmail.com";
+final phone = :"+91 9978437821";
+final location = "Delhi,India";
+
+class Profile extends StatefulWidget { 
+  const Profile({super.key});
+
+@override 
+State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext contrxt) {
+    return Scaffold( 
+      backgroundColor: Colors.white,
+    )
+      minumum: EdgeInsect.
+  }
+  }
+}
+State<profile> createState() => _ProfileState
+
+mixin _ProfileState {
+} {}
